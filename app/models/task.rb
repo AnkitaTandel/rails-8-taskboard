7d569074cb8778a56f94f86b4e903cbbabd601f5
AnkitaT <ankitaatandel@gmail.com>
2026-02-10 14:31:41 +0530

    Adds association and validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :board
+
+  STATUSES = ["todo", "in_progress", "done"]
+
+  validates :title, presence: true
+  validates :status, inclusion: { in: STATUSES }
 end
